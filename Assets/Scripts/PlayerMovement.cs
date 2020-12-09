a376d33bf0d7d23d6467dc4e2c138f52bae67dec
PROTOM <agent0tomek@gmail.com>
2020-12-09 21:43:14 +0100

    Added main project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+
+        Sprint();
+
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("jump");
+    }
+
+    public void Sprint()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("sprint", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("sprint", false);
+        }
+    }
+}
